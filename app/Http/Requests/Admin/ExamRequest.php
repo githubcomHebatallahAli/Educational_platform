2d0a65ed91757d55d1697d319bf480601d9bc19e
Hebatallah Ali <130340820+githubcomHebatallahAli@users.noreply.github.com>
2024-10-23 08:47:46 +0300

    Update ExamRequest.php

--- app/Http/Requests/Admin/ExamRequest.php
+++ app/Http/Requests/Admin/ExamRequest.php
@@ -25,8 +25,7 @@ class ExamRequest extends FormRequest
     {
         return [
             'title' => 'required|string|max:255',
-            'totalMarke' => 'required|integer',
-            'creationDate'=> 'nullable|date_format:Y-m-d',
+            'creationDate'=> 'nullable|date_format:Y-m-d H:i:s',
             'duration' => 'nullable|date_format:H:i:s',
             'numOfQ' => 'required|integer',
             'deadLineExam'  => 'nullable|date_format:Y-m-d H:i:s',
