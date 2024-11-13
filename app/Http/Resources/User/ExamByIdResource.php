a504030963f24c7736d600c1fa9952c4e0038c55
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-11-13 19:32:40 +0200

    update

--- app/Http/Resources/User/ExamByIdResource.php
+++ app/Http/Resources/User/ExamByIdResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\User;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Admin\CourseResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ExamByIdResource extends JsonResource
@@ -14,38 +15,39 @@ class ExamByIdResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $finalExam = $this->exams()->whereNull('lesson_id')->first();
+
 
         return [
-            // 'course' => new CourseResource($this),
-            'lessons' => $this->lessons->map(function ($lesson) {
-                return [
-                    'id' => $lesson->id,
-                    'title' => $lesson->title,
-                    'exam' => $lesson->exam ? [
-                        'id' => $lesson->exam->id,
-                        'title' => $lesson->exam->title,
-                        'duration' => $lesson->exam->duration,
-                        'creationDate' => $lesson->exam->creationDate,
-                        'numOfQ' => $lesson->exam->numOfQ,
-                         'question_order' => $lesson->exam-> question_order,
-                        'formatted_deadLineExam' => $lesson->exam->formatted_deadLineExam,
+            'course' => new CourseResource($this),
+            'id' => $this->id,
+            'title' => $this->title,
 
-                    ] : null,
-                ];
-            }),
-            // إضافة الامتحان غير المرتبط في حال كان موجودًا
-            'final_exam' => $this->exams()->whereNull('lesson_id')->first() ? [
-                'id' => $this->exams()->whereNull('lesson_id')->first()->id,
-                'title' => $this->exams()->whereNull('lesson_id')->first()->title,
-                'duration' => $this->exams()->whereNull('lesson_id')->first()->duration,
-                'creationDate' => $this->exams()->whereNull('lesson_id')->first()->creationDate,
-                'numOfQ' => $this->exams()->whereNull('lesson_id')->first()->numOfQ,
-                'question_order' => $this->exams()->whereNull('lesson_id')->first()->question_order,
-                'formatted_deadLineExam' => $this->exams()->whereNull('lesson_id')->first()->formatted_deadLineExam,
+
+            'exam' => $this->exam ? [
+                'id' => $this->exam->id,
+                'title' => $this->exam->title,
+                'duration' => $this->exam->duration,
+                'creationDate' => $this->exam->creationDate,
+                'numOfQ' => $this->exam->numOfQ,
+                'question_order' => $this->exam->question_order,
+                'formatted_deadLineExam' => $this->exam->formatted_deadLineExam,
+                'questions' => $this->exam->questions->map(function ($question) {
+                    return [
+                        'id' => $question->id,
+                        'question' => $question->question,
+                        'choices' => [
+                            'choice_1' => $question->choice_1,
+                            'choice_2' => $question->choice_2,
+                            'choice_3' => $question->choice_3,
+                            'choice_4' => $question->choice_4,
+                        ],
+                        'correct_choice' => $question->correct_choice,
+                    ];
+                }),
             ] : null,
         ];
+    }
 
 
     }
-}
+
