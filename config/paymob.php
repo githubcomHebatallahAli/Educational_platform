1292a819a557b933c7cd0fcb64215fef155aace3
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-11-26 11:17:57 +0200

    update

--- config/paymob.php
+++ config/paymob.php
@@ -4,6 +4,8 @@ return [
     'card_integration_id' => env('PAYMOB_CARD_INTEGRATION_ID'),
     'api_key' => env('PAYMOB_API_KEY'),
     'hmac' => env('PAYMOB_HMAC'),
+    'secret_key' => env('PAYMOB_SECRET_KEY'),
+    'public_key' => env('PAYMOB_PUBLIC_KEY'),
     'currency' => env('PAYMOB_CURRENCY', 'EGP'),
     // 'iframe_id' => env('PAYMOB_IFRAME_ID'),
 ];
