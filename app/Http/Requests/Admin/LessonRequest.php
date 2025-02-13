8976483b23630ce352823d7d17bfdc75839b25d5
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-02-13 22:04:14 +0200

    up

--- app/Http/Requests/Admin/LessonRequest.php
+++ app/Http/Requests/Admin/LessonRequest.php
@@ -31,7 +31,7 @@ class LessonRequest extends FormRequest
             'description' => 'string|nullable',
             'poster.*'=>'nullable|image|mimes:jpg,jpeg,png,gif,svg',
             // 'video' => 'nullable|mimes:mp4,mov,avi,wmv',
-            'video' =>'nullable|file|mimetypes:video/mp4,video/quicktime|max:1048576',
+            'video' =>'nullable|file|mimetypes:video/mp4,video/quicktime',
             'duration' => 'nullable|date_format:H:i:s',
             'ExplainPdf' => 'nullable|mimes:pdf',
         ];
